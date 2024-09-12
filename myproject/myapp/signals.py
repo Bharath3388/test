92a5a00f2c2aae46e310d0de9bf4179345d15f7e
Bharath3388 <bharathmogalapu@gmail.com>
2024-09-12 11:50:02 +0530

    Add files via upload

--- myproject/myapp/signals.py
+++ myproject/myapp/signals.py
@@ -1,10 +1,8 @@
-import time
+import threading
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
 def my_signal_handler(sender, instance, **kwargs):
-    print("Signal received!")
-    time.sleep(5) 
-    print("Signal processed after delay")
+    print(f"Signal handler thread: {threading.current_thread().name}")
